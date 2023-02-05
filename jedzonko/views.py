0161487d8a3455c0336cdc811814a89293457a5a
BartekJasek <bartek.jasek5@gmail.com>
2023-02-05 21:02:52 +0100

    files

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from django.shortcuts import render
 from django.views import View
+from django.template import loader
+from django.http import HttpResponse
 
 
 class IndexView(View):
@@ -9,3 +11,10 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+
+def homepage(request):
+    template = loader.get_template('index.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
