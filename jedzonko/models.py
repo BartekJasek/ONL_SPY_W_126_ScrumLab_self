7ddc4f21a8c1d54bb737b78c395ab178cbab8534
pailime <emilia.palczyk@gmail.com>
2023-02-05 21:55:30 +0100

    wprowadziłam model Recipe i zrobiłam migracje

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,4 +1,12 @@
+from datetime import datetime
 from django.db import models
 
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=128)
+    ingredients = models.TextField()
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    preparation_time = models.IntegerField()
+    votes = models.IntegerField(default=0)
